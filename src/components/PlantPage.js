e9546bd146eceb6e368d0956dc4fa352c83ff8e0
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-30 11:43:26 -0400

    renders all plants to the page on pageload

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const plantURL = "http://localhost:6001/plants";
 
 function PlantPage() {
+  const [plantData, setPlantData] = useState([]);
+  const fetchData = () => {
+    fetch(plantURL)
+      .then((r) => r.json())
+      .then((data) => {
+        setPlantData(data);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plantData={plantData} />
     </main>
   );
 }
