ab7a407efd22dfaa207664d6d41e0b8e39a54803
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-30 13:26:10 -0400

    toggles out of stock and back on each plant

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
   const { name, image, price } = plant;
+  const [inStock, setInStock] = useState(true);
+
+  function handleClick() {
+    setInStock(!inStock);
+  }
 
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleClick}>
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
